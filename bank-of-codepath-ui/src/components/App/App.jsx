569d20345cde263c1704cba245301a9b90c5c62f
hayley <simmons.hayley15@icloud.com>
2022-06-22 13:28:53 -0500

    Step 2 Finished

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -2,12 +2,29 @@ import * as React from "react"
 import Navbar from "../Navbar/Navbar"
 import Home from "../Home/Home"
 import "./App.css"
+import {BrowserRouter, Router, Routes, Route} from "react-router-dom"
+import TransactionDetail from "../TransactionDetail/TransactionDetail"
+import {useState} from "react"
+
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [transactions, setTransactions] = useState([])
+  const [transfers, setTransfers] = useState([])
+  const [error, setError] = useState(null)
+  const [filterInputValue, setfilterInputValue] = useState("")
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
+    <div className= "app">
+      <BrowserRouter> 
+        <Navbar filterInputValue= {filterInputValue} setfilterInputValue= {setfilterInputValue} />
+        <main> 
+          <Routes> 
+            <Route path= "/"  element= {<Home/>} />
+            <Route path= "/tansactions/:transactionId"  element= {<TransactionDetail/>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
