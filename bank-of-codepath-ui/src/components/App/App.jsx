0e0234082ae399a1877f05cd114cfaa01613081d
hayley <simmons.hayley15@icloud.com>
2022-06-22 16:46:22 -0500

    Step 3 Finished

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -12,16 +12,16 @@ export default function App() {
   const [transactions, setTransactions] = useState([])
   const [transfers, setTransfers] = useState([])
   const [error, setError] = useState(null)
-  const [filterInputValue, setfilterInputValue] = useState("")
+  const [filterInputValue, setFilterInputValue] = useState("")
 
   return (
     <div className= "app">
       <BrowserRouter> 
-        <Navbar filterInputValue= {filterInputValue} setfilterInputValue= {setfilterInputValue} />
+        <Navbar filterInputValue= {filterInputValue} setFilterInputValue= {setFilterInputValue} />
         <main> 
           <Routes> 
             <Route path= "/"  element= {<Home/>} />
-            <Route path= "/tansactions/:transactionId"  element= {<TransactionDetail/>} />
+            <Route path= "/transactions/:transactionId"  element= {<TransactionDetail/>} />
           </Routes>
         </main>
       </BrowserRouter>
